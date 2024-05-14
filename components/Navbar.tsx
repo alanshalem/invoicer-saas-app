7101d616c8d9d460f0392fc06a232907c79705d3
Alan Shalem <alanshalem@gmail.com>
2024-05-14 19:41:57 -0300

    add navbar hamburger and side menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,70 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import ThemeLink from "./ThemeLink";
+import { BiMenu } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const [show, setShow] = useState(false);
+
   return (
-    <header className="bg-violet-700 fixed top-0 right-0 w-full left-0 h-16 flex items-center justify-between px-16 text-slate-50">
-      <Link href="/" className="fpnt-bold text-2xl md:text-4xl">
-        Invoicer
-      </Link>
-      <nav className="flex items-center gap-3">
-        <Link href="/">Features</Link>
-        <Link href="/">Pricing</Link>
-        <Link href="/">Free Tools</Link>
-      </nav>
-      <div className="flex items-center gap-4">
-        <Link href="/login">Login</Link>
-        <ThemeLink
-          className="bg-red-600 hover:bg-red-700  focus:ring-red-300"
-          href="/register"
-          title="Register"
-        />
+    <>
+      {/* TOP MENU */}
+      <header className="bg-violet-700 fixed top-0 right-0 w-full left-0 h-16 flex items-center justify-between px-16 text-slate-50">
+        <Link href="/" className="fpnt-bold text-2xl md:text-4xl">
+          Invoicer
+        </Link>
+        <nav className="hidden sm:flex items-center gap-3">
+          <Link href="/">Features</Link>
+          <Link href="/">Pricing</Link>
+          <Link href="/">Free Tools</Link>
+        </nav>
+        <div className="hidden sm:flex items-center gap-4">
+          <Link href="/login">Login</Link>
+          <ThemeLink
+            className="bg-red-600 hover:bg-red-700  focus:ring-red-300"
+            href="/register"
+            title="Register"
+          />
+        </div>
+        {/* HAMBURGER MENU */}
+        <button onClick={() => setShow(true)} className="sm:hidden">
+          <BiMenu className="text-3xl" />
+        </button>
+      </header>
+      {/* SIDE MENU */}
+      <div
+        className={
+          show
+            ? "sm:hidden fixed w-36 bg-slate-800 bg-opacity-90 h-screen right-0  z-50 top-0 p-4 text-slate-50"
+            : "hidden sm:hidden fixed w-36 bg-slate-800 bg-opacity-90 h-screen right-0  z-50 top-0 p-4 text-slate-50"
+        }
+      >
+        <div className="flex justify-between items-center mb-10">
+          <h2 className="font-bold">Invoicer</h2>
+          <button onClick={() => setShow(false)}>
+            <AiOutlineClose className="text-2xl" />
+          </button>
+        </div>
+        <nav className="flex flex-col items-start gap-3 mb-10">
+          <Link href="/">Features</Link>
+          <Link href="/">Pricing</Link>
+          <Link href="/">Free Tools</Link>
+          {/* {status === "authenticated" && <a href="/invoice">View Invoices</a>} */}
+        </nav>
+        <div className="flex flex-col items-start gap-4">
+          <Link href="/login">Login</Link>
+          <ThemeLink
+            className="bg-rose-600 hover:bg-rose-700 focus:ring-rose-300"
+            title="Register"
+            href="/register"
+          />
+        </div>
       </div>
-    </header>
+    </>
   );
 };
 
